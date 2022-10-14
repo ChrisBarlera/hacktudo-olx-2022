21968e6d8e3912a1fa5ebb47e84236678f77d6dc
Christian Barlera <christian.barlera2@gmail.com>
2022-10-14 19:05:59 -0300

    Finalizando tela de anuncio comum

--- src/pages/anuncios/AnuncioComum.js
+++ src/pages/anuncios/AnuncioComum.js
@@ -9,16 +9,20 @@ function AnunciosComum() {
   return (
     <div className="mainAnunciosContainer">
       <div className="anuncioContent">
-        <p className="cinzaP">Apartamentos para alugar / SC / Aartamento para alugar em BC / Centro / Rua 3000</p>
-        <CadastroTop
-          titulo="Apartamento com 3 quartos para alugar, 100m2 - Centro"
-        />
-        <p className="cinzaP">Apartamento para alugar em</p>
-        <p className="roxoP">Centro, Rua 3000</p>
-        <CadastroTop
-          titulo="R$ 4.000/mês"
-        />
-        <p className="cinzaP">Condomínio R$ 1000 - IPTU R$ 100</p>
+        <div>
+          <p className="cinzaP">Apartamentos para alugar / SC / Aartamento para alugar em BC / Centro / Rua 3000</p>
+          <CadastroTop
+            titulo="Apartamento com 3 quartos para alugar, 100m2 - Centro"
+          />
+          <p className="cinzaP">Apartamento para alugar em</p>
+          <p className="roxoP">Centro, Rua 3000</p>
+        </div>
+        <div>
+          <CadastroTop
+            titulo="R$ 4.000/mês"
+          />
+          <p className="cinzaP">Condomínio R$ 1000 - IPTU R$ 100</p>
+        </div>
         <ul className="apartamentosList">
           <li>
             <LayersIcon/>
